9b44520ea302339bed91e525eaec1f74e8dfb561
JeongSikKim <JeongSikKim@bitbucket.org>
2020-05-21 23:42:47 +0900

    finish indeed get job

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL=f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
     LIMIT = 50
     result = requests.get(URL)
 
@@ -21,16 +21,41 @@ def extract_indeed_pages():
 
     return max_page
 
+def extract_job(html):
+    title = html.find("h2",{"class":"title"}).find("a")["title"]
+    company = html.find("span", {"class":"company"})
+    if company:
+        company_achor = company.find("a")
+        if company_achor is not None:
+            company = str(company_achor.string)
+        else:
+            company = str(company.string)
+        company = company.strip()
+    else:
+        company = None
+    location = html.find("div", {"class":"recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        'link': f"https://kr.indeed.com/%EC%B1%84%EC%9A%A9%EB%B3%B4%EA%B8%B0?jk={job_id}"
+    }
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs=[]
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
-    # for page in range(last_page):
-    # result = requests.get(f"{URL}&start={page*LIMIT}")
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, 'html.parser')
-    results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
-    for result in results:
-        title = result.find("h2",{"class":"title"}).find("a")["title"]
-        print(title)
+
+def get_jbos():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
